b1ef9c7faf39acfee244333566b2d693c66a06e4
Sugayoiya <no_moko@icloud.com>
2024-12-06 11:04:37 +0800

    remove gallery view

--- web/src/app/(app)/workflows/[workflow_id]/@gallery/page.tsx
+++ web/src/app/(app)/workflows/[workflow_id]/@gallery/page.tsx
@@ -1,27 +1,36 @@
-import { LoadingWrapper } from "@/components/LoadingWrapper";
-import { RouteRefresher } from "@/components/RouteRefresher";
-import { Gallery } from "@/components/Gallery";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+"use client";
 
-export default async function Page({
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { LoadingWrapper } from "@/components/LoadingWrapper";
+import { Gallery } from "@/components/Gallery";
+import { RouteRefresher } from "@/components/RouteRefresher";
+
+
+export default function Page({
   params,
+  searchParams,
 }: {
   params: { workflow_id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const workflow_id = params.workflow_id;
+
+  const handleNavigation = () => {
+    window.location.href = `/gallery/${workflow_id}`;
+  };
 
   return (
     <Card className="w-full h-fit min-w-0">
       <CardHeader className="relative">
         <CardTitle>Gallery</CardTitle>
         <div className="absolute right-6 top-6">
-          <RouteRefresher interval={5000} autoRefresh={false} />
+          <RouteRefresher interval={5000} autoRefresh={true} />
         </div>
       </CardHeader>
 
       <CardContent>
         <LoadingWrapper tag="gallery">
-          {/* <Gallery workflowId={workflow_id} /> */}
+          <Gallery workflow_id={workflow_id} searchParams={searchParams}/>
         </LoadingWrapper>
       </CardContent>
     </Card>
--- web/src/app/(app)/workflows/[workflow_id]/layout.tsx
+++ web/src/app/(app)/workflows/[workflow_id]/layout.tsx
@@ -3,15 +3,15 @@ export default async function Layout({
   deployment,
   runs,
   workflow,
-  // gallery,
-  gallery_view,
+  gallery,
+  // gallery_view,
 }: {
   children: React.ReactNode;
   deployment: React.ReactNode;
   runs: React.ReactNode;
   workflow: React.ReactNode;
-  // gallery: React.ReactNode;
-  gallery_view: React.ReactNode;
+  gallery: React.ReactNode;
+  // gallery_view: React.ReactNode;
 }) {
   return (
     <div className="mt-4 w-full grid grid-rows-[1fr,1fr] lg:grid-cols-[minmax(auto,500px),1fr] gap-4 max-h-[calc(100dvh-100px)]">
@@ -21,8 +21,8 @@ export default async function Layout({
       </div>
       <div className="flex flex-col gap-4">
         {runs}
-        {/* {gallery} */}
-        {gallery_view}
+        {gallery}
+        {/* {gallery_view} */}
       </div>
       {children}
     </div>
